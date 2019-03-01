b054a8d61a32e53b596b2ce8d8b12621980d1ac7
Razvan Veina <razvan.veina@ssi-schaefer.com>
2019-03-01 11:38:05 +0200

    Add Test class

---
+++ Implementation/Team1_Farmacie/src/Test.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 SSI Schaefer Noell GmbH
+ *
+ * $Header: $
+ */
+
+/**
+ * @author <a href="mailto:rveina@ssi-schaefer-noell.com">rveina</a>
+ * @version $Revision: $, $Date: $, $Author: $
+ */
+
+public class Test {
+
+}
